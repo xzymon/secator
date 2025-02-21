4bc173b1bc57744299b8814d1ad8ab91ac221e32
xzymon <xzymon@gmail.com>
2025-02-21 21:56:05 +0100

    Initial

---
+++ src/main/java/com/xzymon/secator/SecatorApplication.java
@@ -0,0 +1,13 @@
+package com.xzymon.secator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xzymon/secator/SecatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzymon.secator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
